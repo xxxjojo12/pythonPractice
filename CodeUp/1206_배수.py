cadbca0a6fa2982ccdf43ae6cc75cc263dfe97e1
xxxjojo12 <dlagkdlf12@gmail.com>
2022-05-30 02:07:39 +0900

    Update 1206_배수.py

--- CodeUp/1206_배수.py
+++ CodeUp/1206_배수.py
@@ -1,2 +1,12 @@
 a, b = map(int, input().split())
 
+if a % b == 0 and a > b:
+    n = a//b
+    print(f'{b}*{n}={a}')
+elif b % a == 0 and b > a:
+    n = b//a
+    print(f'{a}*{n}={b}')
+elif a == b:
+    print(f'{a}*{1}={b}')
+else:
+    print("none")
