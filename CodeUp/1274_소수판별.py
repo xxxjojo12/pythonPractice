8d3f8d8e7dce3d63ba7651ebf52c0d054c492009
xxxjojo12 <dlagkdlf12@gmail.com>
2022-06-05 05:59:04 +0900

    Update 1274_소수판별.py

--- CodeUp/1274_소수판별.py
+++ CodeUp/1274_소수판별.py
@@ -1,9 +1,10 @@
 n = int(input())
 
-for i in range(2, n+1):
-    if n % i == 0:
+for i in range(2, n):
+    if n % i == 0 and n != 2:
         print("not prime")
         break
-    else:
-        print("prime")
-        break
+        
+else:
+    print("prime")
+    
