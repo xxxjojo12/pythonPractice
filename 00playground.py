1e93d9c8b116ac797eb8726c819c051f6ffa82e1
LinLin <dlagkdlf12@gmail.com>
2022-06-12 05:36:40 +0900

    Create 00playground.py

---
+++ 00playground.py
@@ -0,0 +1,65 @@
+# Tuple
+a = (1, 2, 'a', (3, 4), 5)
+# del a[0]
+print(a[0:3])
+print(a[4])
+
+print('--------------------------------------')
+
+# Dictionary (key : value) -> items
+b = {
+    'Hail' : 1,
+    'Lim' : 2,
+    'age' : 29
+}
+print(b['age'])
+b['old'] = 30
+print(b)
+
+del b['Lim']
+print(b)
+
+print(b.keys())
+print(b.values())
+
+print(b.items())
+
+print(b.clear())
+
+# Two lists into one dictionary
+l1 = ['apple', 'banana', 'orange']
+l2 = [1, 2, 3]
+d1 = {x : y for x, y in zip(l1, l2)}
+print(d1)
+
+print('--------------------------------------')
+
+# Casting 형변환
+print(float(3))
+print(int(3.1))
+print(str(4))
+print(hex(12))
+print(oct(10))
+print(bin(10))
+
+# list to tuple
+print(tuple([1, 2]))
+
+# tuple to list
+print(list((3, 4)))
+
+# list to 집합 데이터형
+print(set([5, 6]))
+
+# Two list to dictionary
+print(dict([['a', 1], ['b', 2]]))  
+
+# Unicode
+print(ord('a'))
+print(chr(97))
+
+print('--------------------------------------')
+
+# global and local
+a = 1
+b = 2
