83510cf676f9bdb3105b2878765f51271b1d1eee
xxxjojo12 <dlagkdlf12@gmail.com>
2022-06-11 09:14:07 +0900

    replaceRepo

--- LeetCode/palindrome.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-	        return False
-        return str(x) == str(x)[::-1]
-
-
-a = Solution()
-print(a.isPalindrome(121))
